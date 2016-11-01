d430d705018accc62b6ad5818c326ee454f8e495
Valera Horsharik <valera.horsharik@gmail.com>
2016-11-01 20:03:08 +0200

    Change USERS table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('surname');
+            $table->string('nick_name');
+            $table->date('birth_date');
             $table->string('email')->unique();
             $table->string('password');
             $table->char('role',15)->default('User');
