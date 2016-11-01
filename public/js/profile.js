7433c2b5896d442cb3957ad4351ce84c666c27df
Valera Horsharik <valera.horsharik@gmail.com>
2016-11-01 20:04:10 +0200

    Add update birth date

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,27 +1,43 @@
 $(document).ready(function () {
+    /*
+     * int year,month
+     * return int
+     * function return number of days in current month in current Year
+     */
     Date.prototype.daysInMonth = function (year, month) {
         return 32 - new Date(year, month - 1, 32).getDate();
     };
-    $('select[name="b-month"]').on('change', function () {
-        var years = $('select[name="b-year"] option:selected').attr('value');
-        var month = $('select[name="b-month"] option:selected').attr('value');
-        var days = new Date().daysInMonth(years, month);
-        $('select[name="b-day"] option').remove();
-        for (var i = 1; i <= days; i++) {
+    var years = $('select[name="b-year"] option:selected').attr('value');
+    var month = $('select[name="b-month"] option:selected').attr('value');
+    var days = new Date().daysInMonth(years, month);
+    var selectedDay = $('select[name="b-day"] option').val();
+    $('select[name="b-day"] option').remove();
+    for (var i = 1; i <= days; i++) {
+        if (i == selectedDay) {
+            $('select[name="b-day"]').append('<option value="' + i + '" selected>' + i + '</option>');
+        } else {
             $('select[name="b-day"]').append('<option value="' + i + '">' + i + '</option>');
         }
-    });
+    }
+
     $('#b-date').on('change', function () {
-//        $.ajaxSetup({
-//            headers: {
-//                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-//            }
-//        })
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        })
         var year = $('select[name="b-year"] option:selected').attr('value');
         var month = $('select[name="b-month"] option:selected').attr('value');
         var day = $('select[name="b-day"] option:selected').attr('value');
-        console.log('Selected day = ' + day + ';Selected month = ' + month + ';Selected year = ' + year);
-
+        var days = new Date().daysInMonth(years, month);
+        $('select[name="b-day"] option').remove();
+        for (var i = 1; i <= days; i++) {
+            if (i == day) {
+                $('select[name="b-day"]').append('<option value="' + i + '" selected>' + i + '</option>');
+            } else {
+                $('select[name="b-day"]').append('<option value="' + i + '">' + i + '</option>');
+            }
+        }
         $.ajax({
             type: "PUT",
             url: "/profile/bdate/",
@@ -30,8 +46,8 @@ $(document).ready(function () {
                 month: month,
                 day: day
             },
-            success: function () {
-                $('#b-date').append('<div class="col-md-4 col-md-offset-4 col-xs-6 col-xs-offset-3 success">Your birthday updated!</div>');
+            success: function (data) {
+                $('#b-date').append('<div class="col-md-4 col-md-offset-4 col-xs-6 col-xs-offset-3 success">' + data + '</div>');
             }
         });
     });
