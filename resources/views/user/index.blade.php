7ad35c6dd4ec9bb41943b6eec6d4516978d2141b
Valera Horsharik <valera.horsharik@gmail.com>
2016-11-01 20:05:31 +0200

    Change view of profile

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,29 +2,44 @@
 @section('title','Personal page')
 @section('content')
 <div class='container'>
-    <div class='col-md-8'>
-        <h1>Its your's personal page,{{$user->name}}!</h1>
+    <div class="row">
+        <div class='col-md-6 col-md-offset-3'>
+            <h1>Its your's personal page,{{$user->name}}!</h1>
+        </div>
+    </div>
+    <div class="row">
+        <!--<div class="col-md-4" >-->
+
         <div id='b-date'>
+            Birthdate:
             <select name="b-day">
-                <option value='1'>1</option>
+                <option value='{{$date['day']}}'>{{$date['day']}}</option>
             </select>
             <select name="b-month">
                 @foreach($months as $month)
-                <option value="{{$loop->iteration}}">{{$month}}</option>
+                    @if($loop->iteration == $date['month'])
+                        <option value="{{$loop->iteration}}" selected>{{$month}}</option>
+                    @else
+                        <option value="{{$loop->iteration}}">{{$month}}</option>
+                    @endif
                 @endforeach
             </select>
             <select name='b-year'>
                 @for ($i = date('Y')-70;$i <= date('Y');$i++)
-                <option value='{{$i}}'>{{$i}}</option>
+                    @if($i == $date['year'])
+                        <option value='{{$i}}' selected>{{$i}}</option>
+                    @else
+                        <option value='{{$i}}'>{{$i}}</option>
+                    @endif
                 @endfor
             </select>
-        </div>   
-        <button id='updateBday'>Save</button>
+            <!--</div>-->   
+        </div>
     </div>
+
 </div>
 @endsection
 
 @section('scripts')
-<!-- <meta name="_token" content="{!! csrf_token() !!}" />-->
 <script src='/public/js/profile.js'></script>
 @endsection
