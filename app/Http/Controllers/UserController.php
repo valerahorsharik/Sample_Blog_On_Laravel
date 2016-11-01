0e3782dfa5c59e65b863e203f755502e2e52c6aa
Valera Horsharik <valera.horsharik@gmail.com>
2016-11-01 19:59:00 +0200

    Add birthDate update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,25 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\User;
 use \Illuminate\Support\Facades\Auth;
-class UserController extends Controller
-{
+
+class UserController extends Controller {
+
     public function __construct() {
         $this->middleware('auth');
     }
-    
-    public function index(){
-        $user = User::find(Auth::user()->id);
-        $months = ['January','February','March','April','May',
-            'Jun','July','August','September','October','November'
-            ,'December',];
-        return view('user.index',['user'=>$user,'months'=>$months]);
+
+    public function index() {
+        $user = Auth::user();
+        $date = date_parse_from_format('Y-m-d', Auth::user()->birth_date);
+//        var_dump($date);
+//        die;
+        $months = ['January', 'February', 'March', 'April', 'May',
+            'Jun', 'July', 'August', 'September', 'October', 'November'
+            , 'December',];
+        return view('user.index', ['user' => $user,
+            'months' => $months,
+            'date'=>$date,
+            ]);
     }
-    
-    public function bdayUpdate(Request $response) {
-       echo "123";
+
+    public function updateBirthDate(Request $request) {
+        $birthDate = $request->year. '-' . $request->month . '-' .$request->day;      
+        Auth::user()->birth_date = $birthDate;
+        Auth::user()->save();
     }
+
 }
