2cbc3e96fa5da3f6c81ef39e746018eedfb9bca7
Valera Horsharik <valera.horsharik@gmail.com>
2016-11-02 22:08:59 +0200

    Change personal page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,38 +3,59 @@
 @section('content')
 <div class='container'>
     <div class="row">
+        <div class="col-md-4 col-md-offset-4" id="message-profile"></div>
+    </div>
+    <div class="row">
         <div class='col-md-6 col-md-offset-3'>
-            <h1>Its your's personal page,{{$user->name}}!</h1>
+            <h1>Its your's personal page,{{$user->nick_name}}!</h1>
         </div>
     </div>
     <div class="row">
-        <!--<div class="col-md-4" >-->
-
-        <div id='b-date'>
-            Birthdate:
-            <select name="b-day">
-                <option value='{{$date['day']}}'>{{$date['day']}}</option>
-            </select>
-            <select name="b-month">
-                @foreach($months as $month)
-                    @if($loop->iteration == $date['month'])
-                        <option value="{{$loop->iteration}}" selected>{{$month}}</option>
-                    @else
-                        <option value="{{$loop->iteration}}">{{$month}}</option>
-                    @endif
-                @endforeach
-            </select>
-            <select name='b-year'>
-                @for ($i = date('Y')-70;$i <= date('Y');$i++)
-                    @if($i == $date['year'])
-                        <option value='{{$i}}' selected>{{$i}}</option>
-                    @else
-                        <option value='{{$i}}'>{{$i}}</option>
-                    @endif
-                @endfor
-            </select>
-            <!--</div>-->   
+        <div class="col-md-2 col-md-offset-3">
+            Avatar
         </div>
+        <div class="col-md-4">
+            <div id="name">
+                Name:<input type="text" class="data" value="{{$user->name}}"/>
+            </div>
+            <div id="surname">
+                Surname:<input type="text" class="data" value="{{$user->surname}}"/>
+            </div>
+            <div id="email">
+                Email:<input type="email" class="data" value="{{$user->email}}"/>
+            </div>
+            <div id="role">
+                Role:{{$user->role}}
+            </div>
+            <div id="activity">
+                Activity:{{$user->posts->count()}}
+            </div>
+            <div id='b-date'>
+                Birthdate:
+                <select name="b-day" class="data">
+                    <option value='{{$date['day']}}'>{{$date['day']}}</option>
+                </select>
+                <select name="b-month" class="data">
+                    @foreach($months as $month)
+                        @if($loop->iteration == $date['month'])
+                            <option value="{{$loop->iteration}}" selected>{{$month}}</option>
+                        @else
+                            <option value="{{$loop->iteration}}">{{$month}}</option>
+                        @endif
+                    @endforeach
+                </select>
+                <select name="b-year" class="data">
+                    @for ($i = date('Y')-70;$i <= date('Y');$i++)
+                        @if($i == $date['year'])
+                            <option value='{{$i}}' selected>{{$i}}</option>
+                        @else
+                            <option value='{{$i}}'>{{$i}}</option>
+                        @endif
+                    @endfor
+                </select> 
+            </div>
+        </div>
+
     </div>
 
 </div>
