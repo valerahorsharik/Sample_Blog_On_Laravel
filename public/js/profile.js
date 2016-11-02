f79fa5c39816994f4124ee9397c28183cc61ab8b
Valera Horsharik <valera.horsharik@gmail.com>
2016-11-02 22:07:56 +0200

    Developing auto update

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
     /*
-     * int year,month
+     * int year
+     * int month
      * return int
      * function return number of days in current month in current Year
      */
     Date.prototype.daysInMonth = function (year, month) {
         return 32 - new Date(year, month - 1, 32).getDate();
     };
+    var data = {};
+
+    /*
+     * generate amout of days depends on current month/year on document load
+     */
     var years = $('select[name="b-year"] option:selected').attr('value');
     var month = $('select[name="b-month"] option:selected').attr('value');
     var days = new Date().daysInMonth(years, month);
@@ -19,36 +25,44 @@ $(document).ready(function () {
             $('select[name="b-day"]').append('<option value="' + i + '">' + i + '</option>');
         }
     }
-
+    
+    /*
+     * generate amout of days depends on current month/year on date change
+     */
     $('#b-date').on('change', function () {
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        })
-        var year = $('select[name="b-year"] option:selected').attr('value');
-        var month = $('select[name="b-month"] option:selected').attr('value');
-        var day = $('select[name="b-day"] option:selected').attr('value');
-        var days = new Date().daysInMonth(years, month);
+        data['year'] = $('select[name="b-year"] option:selected').attr('value');
+        data['month'] = $('select[name="b-month"] option:selected').attr('value');
+        data['day'] = $('select[name="b-day"] option:selected').attr('value');
+        var days = new Date().daysInMonth(data['year'], data['month']);
         $('select[name="b-day"] option').remove();
         for (var i = 1; i <= days; i++) {
-            if (i == day) {
+            if (i == data['day']) {
                 $('select[name="b-day"]').append('<option value="' + i + '" selected>' + i + '</option>');
             } else {
                 $('select[name="b-day"]').append('<option value="' + i + '">' + i + '</option>');
             }
         }
+    });
+    function updateEveryThing() {
+        console.log(data);
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
         $.ajax({
             type: "PUT",
             url: "/profile/bdate/",
-            data: {
-                year: year,
-                month: month,
-                day: day
-            },
-            success: function (data) {
-                $('#b-date').append('<div class="col-md-4 col-md-offset-4 col-xs-6 col-xs-offset-3 success">' + data + '</div>');
+            data: data,
+            success: function () {
+                $('#message-profile').html('Success');
             }
         });
+    }
+    var timerId = 0;
+    $('.data').on('change', function () {
+        data[$(this).parent().attr('id')] = $(this).val();
+        clearTimeout(timerId);
+        timerId = setTimeout(updateEveryThing, 5000);
     });
 });
