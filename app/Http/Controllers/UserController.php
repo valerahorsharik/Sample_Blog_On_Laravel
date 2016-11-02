5cb60ce4ab38ff28f32c6b9a242651f5f5528463
Valera Horsharik <valera.horsharik@gmail.com>
2016-11-02 23:51:39 +0200

    add update personal data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,21 +16,29 @@ class UserController extends Controller {
     public function index() {
         $user = Auth::user();
         $date = date_parse_from_format('Y-m-d', Auth::user()->birth_date);
-//        var_dump($date);
-//        die;
         $months = ['January', 'February', 'March', 'April', 'May',
-            'Jun', 'July', 'August', 'September', 'October', 'November'
-            , 'December',];
-        return view('user.index', ['user' => $user,
+            'Jun', 'July', 'August', 'September', 'October', 'November',
+            'December',];
+        return view('user.index', [
+            'user' => $user,
             'months' => $months,
             'date' => $date,
         ]);
     }
 
-    public function updateBirthDate(Request $request) {
+    public function update(Request $request) {
         if (isset($request->year) && isset($request->month) && isset($request->day)) {
             $birthDate = $request->year . '-' . $request->month . '-' . $request->day;
             Auth::user()->birth_date = $birthDate;
+        }
+        if (isset($request->email)) {
+            Auth::user()->email = $request->email;
+        }
+        if (isset($request->name)) {
+            Auth::user()->name = $request->name;
+        }
+        if (isset($request->surname)) {
+            Auth::user()->surname = $request->surname;
         }
 
         Auth::user()->save();
