d2712f0fc02aed31f2f7a40f46943f237794eb0e
Valera Horsharik <valera.horsharik@gmail.com>
2016-12-21 22:22:23 +0200

    added a link for user's page and style for that page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
-@section('title','Post {{$post->title}}')
+@section('title')
+Post {{$post->title}}
+@endsection
+@section('styles')
+    <link href="/public/css/posts.css" rel="stylesheet">
+@endsection
 @section('content')
 <p>{{$post->title}}</p>
 <p>{{$post->article}}</p>
 <p>{{$post->created_at}}</p>
-<p>{{$post->user->name}}</p>
+<p><a href="/profile/{{$post->user->nick_name}}">{{$post->user->name}}</a></p>
 @endsection
 
