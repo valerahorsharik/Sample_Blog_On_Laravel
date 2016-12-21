57ef5fa2dad15dbabd2c2b874141375b8a99979b
Valera Horsharik <valera.horsharik@gmail.com>
2016-12-21 22:19:43 +0200

    check if isset errors

--- resources/views/errors/errors.blade.php
+++ resources/views/errors/errors.blade.php
@@ -1,14 +1,16 @@
-@if (count($errors) > 0)
-  <!-- Список ошибок формы -->
-  <div class="alert alert-danger">
-    <strong>Oops! Something going wrong...</strong>
+@if (isset($errors))
+    @if (count($errors) > 0)
+      <!-- Список ошибок формы -->
+      <div class="alert alert-danger">
+        <strong>Oops! Something going wrong...</strong>
 
-    <br><br>
+        <br><br>
 
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  </div>
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
 @endif
