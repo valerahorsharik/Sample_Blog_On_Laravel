48ec168eeec59a9bd988bc3ababef2e91d1102da
Valera Horsharik <valera.horsharik@gmail.com>
2016-12-21 22:21:07 +0200

    Common commit, small changed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,20 +1,44 @@
 @extends('layouts.app')
 @section('title','Posts')
+@section('styles')
+    <link href="/public/css/posts.css" rel="stylesheet">
+@endsection
 @section('content')
 @include('errors.errors')
-<div class="container">
+<div class="container posts-main-container">
     @foreach($posts as $post)
 
-    <div class="col-md-8 col-md-offset-2 col-xs-8 col-xs-offset-2" style="word-wrap: break-word; ">
-        <a href="/posts/{{$post->id}}"><h3>{{$post->title}}</h1></a>
-        {{$post->article}}
-        <div class="col-md-offset-8  col-xs-offset-6" style="text-align: right">
-            Author:{{$post->user->name}}
+    <div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1 post">
+        <div class="row"><a href="/post/{{$post->id}}"><h3>{{$post->title}}</h3></a></div>
+        <div class="row">{{$post->article}}</div>
+        <div class="row">
+            <div class="col-md-2 options">
+                @if(Auth::check())
+                    @if ($post->user_id == Auth::user()->id)
+                        <a class="delete_post_href" href="/post/delete/{{$post->id}}">x</a>
+                        <a class="delete_post_href" href="/post/edit/{{$post->id}}">u</a>
+                    @endif
+                @endif
+            </div>
+            <div class="col-md-4  col-md-offset-6 text-right">
+                Author:{{$post->user->name}}
+            </div>
+
         </div>
-        <hr/>
+        
+        <div class="row">
+            <div class="col-md-2 col-md-offset-5" data-post-id="{{$post->id}}">
+                
+            </div>
+        
+        </div>
+            
     </div>
 
     @endforeach
 </div>
+@endsection
+@section('scripts')
+<script src='/public/js/posts.js'></script>
 @endsection
 
