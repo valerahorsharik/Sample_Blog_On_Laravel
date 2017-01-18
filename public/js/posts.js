4b5c75b5df6c5f22cb79313c37201c3ca6107197
valerahorsharik <valera.horsharik@gmail.com>
2017-01-18 22:37:37 +0200

    Added a documentation.

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
+    /*
+     * Setup CSRF token for all ajax requests
+     */
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-
+    
+    /*
+     * Download comments using ajax by postId
+     */
     $('.download-comments').on('click', function () {
-        // download comments using ajax
         if ($(this).parent().find('.comments').length === 0) {
             var postId = $(this).attr('data-post-id');
             $.ajax({
@@ -25,7 +30,9 @@ $(document).ready(function () {
         }
         $(this).removeClass('download-comments');
     });
-
+    /*
+     * Show/hide comments if they are already downloaded
+     */
     $('.toggle-comments').on('click', function () {
         if (!$(this).hasClass('download-comments')) {
             $(this).toggleClass('active');
