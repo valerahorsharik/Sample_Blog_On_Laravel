01475fc5194e365e7419d1f8cee5d30d24fa73b9
valerahorsharik <valera.horsharik@gmail.com>
2017-01-18 23:31:36 +0200

    Remake a view for displaying single post and added opportunity for review comments.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,12 +3,38 @@
 Post {{$post->title}}
 @endsection
 @section('styles')
-    <link href="/public/css/posts.css" rel="stylesheet">
+<link href="/public/css/posts.css" rel="stylesheet">
 @endsection
 @section('content')
-<p>{{$post->title}}</p>
-<p>{{$post->article}}</p>
-<p>{{$post->created_at}}</p>
-<p><a href="/profile/{{$post->user->nick_name}}">{{$post->user->name}}</a></p>
-@endsection
+<div class="container">
+    <div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1 post">
+        <div class="row"><h3>{{$post->title}}</h3></div>
+        <div class="row">{{$post->article}}</div>
+        <div class="row">
+            <div class="col-md-2 options">
+                @if(Auth::check())
+                    @if ($post->user_id == Auth::user()->id)
+                        <a class="delete_post_href" href="/post/delete/{{$post->id}}">x</a>
+                        <a class="delete_post_href" href="/post/edit/{{$post->id}}">u</a>
+                    @endif
+                @endif
+            </div>
+            <div class="col-md-4  col-md-offset-6 text-right">
+                <a href="/profile/{{$post->user->nick_name}}">{{$post->user->name}}</a> {{$post->created_at}}
+            </div>
+        </div>
+        <div class="row"> 
+            <div class="download-comments toggle-comments"  data-post-id="{{$post->id}}">
+                @if ($post->comments_count > 0)
+                    Show comments
+                @else
+                    Add first comment
+                @endif 
+            </div>
+        </div>
+    </div>
 
+    @endsection
+    @section('scripts')
+    <script src='/public/js/posts.js'></script>
+    @endsection
