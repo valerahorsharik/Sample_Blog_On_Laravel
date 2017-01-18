3c82c601486317af5038acbc50a3a5441f2b32f3
valerahorsharik <valera.horsharik@gmail.com>
2017-01-18 23:18:48 +0200

    Remake view for comments.

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -1,7 +1,12 @@
-<ul class="comments">
-    @foreach($comments as $comment)
-    <li>
-        {{$comment->text}}
-    </li>
-    @endforeach
-</ul>
+<div class="comments">
+    @include('comments.addForm')
+    @if (count($comments) > 0)
+    <ul class="comments">
+        @foreach($comments as $comment)
+        <li>
+            {{$comment->text}}
+        </li>
+        @endforeach
+    </ul>
+    @endif    
+</div>
