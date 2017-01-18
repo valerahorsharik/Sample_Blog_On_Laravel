13d22f2ec28e5f8cb64200c8bc1ef56d24e5cf6a
valerahorsharik <valera.horsharik@gmail.com>
2017-01-18 23:28:05 +0200

    Added an icon for comments and removed option to review comments.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -26,9 +26,7 @@
             <div class="col-md-4  col-md-offset-1 text-right">
                 Author:{{$post->author}}
             </div>
-        </div>
-       
-            
+        </div>         
     </div>
 
     @endforeach
