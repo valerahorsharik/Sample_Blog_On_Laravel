3f557332764b377c1f7da66308e61d94c4300537
valerahorsharik <valera.horsharik@gmail.com>
2017-01-19 19:46:18 +0200

    Added a store method.

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -19,5 +19,20 @@ class CommentController extends Controller {
         }
         return redirect()->route('post.index')->withErrors('Sorry, but you are doing wrong...');
     }
-
+    
+    /**
+     * Saves comment from request
+     * 
+     * @param Request $request
+     * 
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request){
+        $request->user()->comments()->create([
+           'text' => $request->text,
+            'post_id' => 7
+        ]);
+        
+        return redirect()->back();
+    }
 }
