4da8ebc8f3ffd4b90b19b39bf31f09ed5ffe80d8
valerahorsharik <valera.horsharik@gmail.com>
2017-01-19 19:47:55 +0200

    Testing saving with simple form.

--- resources/views/comments/addForm.blade.php
+++ resources/views/comments/addForm.blade.php
@@ -1,4 +1,5 @@
-<form class="comments-form" method="POST">
-    <textarea rows="3" placeholder="Input you comment..."></textarea>
+<form class="comments-form" method="POST" action="/comment/save">
+    {{csrf_field()}}
+    <textarea rows="3" placeholder="Input you comment..." name="text"></textarea>
     <input type="submit" value="Send" name="send">
 </form>
