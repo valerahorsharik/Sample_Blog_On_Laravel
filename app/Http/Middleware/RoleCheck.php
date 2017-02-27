bba76cbaad76ae86211edc3354add7331bd1be9d
valerahorsharik <valera.horsharik@gmail.com>
2017-02-27 17:24:41 +0200

    small changes

--- app/Http/Middleware/RoleCheck.php
+++ app/Http/Middleware/RoleCheck.php
@@ -1,24 +1,14 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace App\Http\Middleware;
 
 use Closure;
 use \Illuminate\Support\Facades\Auth;
 
-/**
- * Description of RoleCheck
- *
- * @author valeriycss
- */
+
 class RoleCheck {
 
-    //put your code here
+    
     public function handle($request, Closure $next) {
         $role = $request->user()->role;
         if ($role != 'admin') {
